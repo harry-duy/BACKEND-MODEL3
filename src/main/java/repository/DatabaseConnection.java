1a7ccd97e5ae5db03d49d9acaa92282ffa76e07c
harry-duy <ngothanhduy04@gmail.com>
2025-02-18 19:22:40 +0700

    push

--- src/main/java/repository/DatabaseConnection.java
+++ src/main/java/repository/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/bookstoredb";
     private static final String USER = "root";
-    private static final String PASSWORD = "159357bapD";
+    private static final String PASSWORD = "admin4320";
 
     public static Connection getConnection() {
         try {
